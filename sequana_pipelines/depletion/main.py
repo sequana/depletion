7516e2784cece3ab21916fe3430c116ebcfd6d22
Thomas Cokelaer <cokelaer@gmail.com>
2023-12-16 23:32:19 +0100

    Convert to pyproject and click usage

--- sequana_pipelines/depletion/main.py
+++ sequana_pipelines/depletion/main.py
@@ -12,101 +12,68 @@
 ##############################################################################
 import sys
 import os
-import argparse
 import shutil
 import subprocess
 
-from sequana_pipetools.options import *
-from sequana_pipetools.options import before_pipeline
-from sequana_pipetools.misc import Colors
-from sequana_pipetools.info import sequana_epilog, sequana_prolog
-from sequana_pipetools import SequanaManager
 
-col = Colors()
+import click_completion
+import rich_click as click
+from sequana_pipetools import SequanaManager
+from sequana_pipetools.options import *
+
+click_completion.init()
 
 NAME = "depletion"
 
 
-class Options(argparse.ArgumentParser):
-    def __init__(self, prog=NAME, epilog=None):
-        usage = col.purple(sequana_prolog.format(**{"name": NAME}))
-        super(Options, self).__init__(
-            usage=usage,
-            prog=prog,
-            description="",
-            epilog=epilog,
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        )
-        # add a new group of options to the parser
-        so = SlurmOptions()
-        so.add_options(self)
-
-        # add a snakemake group of options to the parser
-        so = SnakemakeOptions(working_directory=NAME)
-        so.add_options(self)
-
-        so = InputOptions()
-        so.add_options(self)
-
-        so = GeneralOptions()
-        so.add_options(self)
-
-        pipeline_group = self.add_argument_group("pipeline")
-
-        pipeline_group.add_argument("--mode", dest="mode", required=True, choices=["selection", "depletion"])
-        pipeline_group.add_argument("--reference", dest="reference", required=True, 
-            help="reference from which to select or deplete reads")
-
-        self.add_argument("--run", default=False, action="store_true", help="execute the pipeline directly")
-
-    def parse_args(self, *args):
-        args_list = list(*args)
-        if "--from-project" in args_list:
-            if len(args_list) > 2:
-                msg = (
-                    "WARNING [sequana]: With --from-project option, "
-                    + "pipeline and data-related options will be ignored."
-                )
-                print(col.error(msg))
-            for action in self._actions:
-                if action.required is True:
-                    action.required = False
-        options = super(Options, self).parse_args(*args)
-        return options
+help = init_click(
+    NAME,
+    groups={
+        "Pipeline Specific": [
+            "--mode",
+            "--reference-file",
+        ],
+    },
+)
 
 
-def main(args=None):
+@click.command(context_settings=help)
+@include_options_from(ClickInputOptions)
+@include_options_from(ClickSnakemakeOptions, working_directory=NAME)
+@include_options_from(ClickSlurmOptions)
+@include_options_from(ClickGeneralOptions)
+@click.option("--mode", 
+    "mode", 
+    required=True, 
+    type=click.Choice(["selection", "depletion"]))
+@click.option("--reference-file", 
+    "reference", required=True, 
+    type=click.Path(dir_okay=False, file_okay=True),
+       help="reference from which to select or deplete reads")
 
-    if args is None:
-        args = sys.argv
 
-    # whatever needs to be called by all pipeline before the options parsing
-    before_pipeline(NAME)
+def main(**options):
 
-    # option parsing including common epilog
-    options = Options(NAME, epilog=sequana_epilog).parse_args(args[1:])
+    if options["from_project"]:
+        click.echo("--from-project Not yet implemented")
+        sys.exit(1)
 
     # the real stuff is here
     manager = SequanaManager(options, NAME)
-
-    # create the beginning of the command and the working directory
     manager.setup()
 
-    # fill the config file with input parameters
-    if options.from_project is None:
-        cfg = manager.config.config
-        cfg.input_directory = os.path.abspath(options.input_directory)
-        cfg.input_pattern = options.input_pattern
-        manager.exists(cfg.input_directory)
-        cfg.general.mode = options.mode
-        cfg.general.reference = os.path.abspath(options.reference)
+    # aliases
+    options = manager.options
+    cfg = manager.config.config
+
+    manager.fill_data_options()
+
+    cfg.general.mode = options.mode
+    cfg.general.reference = os.path.abspath(options.reference)
 
     # finalise the command and save it; copy the snakemake. update the config
     # file and save it.
     manager.teardown()
-
-    if options.run:
-        subprocess.Popen(["sh", "{}.sh".format(NAME)], cwd=options.workdir)
 
 
 if __name__ == "__main__":
